2f1f6f9114e7e4bbdba6b39dfefd881ffc358049
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-06-24 16:44:49 +0900

    シフト登録ページのチェックボックスで書き込み可、不可判定を行った

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
+$(function(){
+    $(".back").on("click",function(ev){
+        location.href='./../';
+    });
+
+
+
 $("[name='shift_checkbox']").bootstrapSwitch();
       $('input[name="shift_checkbox"]').on({'switchChange.bootstrapSwitch': function(event, state) {
       if(state == true ){
         console.log("可");
-         flag = 1;
+         $(".time").attr('disabled', false);
       } else {
         console.log("不可");
          flag = 0;
+         $(".time").attr('disabled', true);
+         
       }
     }
   });
+    });
