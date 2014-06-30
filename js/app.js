dba72a5fb0ddcee922340d0ca5ad2a75d4a528e5
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-06-30 14:07:50 +0900

    途中

--- js/app.js
+++ js/app.js
@@ -3,13 +3,19 @@ $(function(){
         location.href='./../';
     });
 
-
-
 $("[name='shift_checkbox']").bootstrapSwitch();
-      $('input[name="shift_checkbox"]').on({'switchChange.bootstrapSwitch': function(event, state) {
+    });
+
+//シフト登録ページで色を金にする。
+$("tbody th").on("click",function(){
+  $("#date").html($(this).text());
+  $(this).attr("class","in");
+  console.log($(event.target).index());
+$('input[name="shift_checkbox"]').on({'switchChange.bootstrapSwitch': function(event, state) {
       if(state == true ){
         console.log("可");
          $(".time").attr('disabled', false);
+              
       } else {
         console.log("不可");
          flag = 0;
@@ -18,4 +24,7 @@ $("[name='shift_checkbox']").bootstrapSwitch();
       }
     }
   });
-    });
+});
+
+
+      
