696203188cc2809d391f0ef9ca712c8a961f62a8
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-07-07 14:40:27 +0900

    色付け問題解決

--- js/app.js
+++ js/app.js
@@ -1,30 +1,39 @@
 $(function(){
-    $(".back").on("click",function(ev){
-        location.href='./../';
-    });
-
-$("[name='shift_checkbox']").bootstrapSwitch();
-    });
-
-//シフト登録ページで色を金にする。
-$("tbody th").on("click",function(){
-  $("#date").html($(this).text());
-  $(this).attr("class","in");
-  console.log($(event.target).index());
-$('input[name="shift_checkbox"]').on({'switchChange.bootstrapSwitch': function(event, state) {
-      if(state == true ){
-        console.log("可");
-         $(".time").attr('disabled', false);
-              
-      } else {
-        console.log("不可");
-         flag = 0;
-         $(".time").attr('disabled', true);
-         
-      }
-    }
+  $(".back").on("click",function(ev){
+    location.href='./../';
   });
+
+  $("[name='shift_checkbox']").bootstrapSwitch();
+  
+
 });
 
 
-      
+
+
+//シフト登録ページで色を金にする。
+$("tbody th").on("click",function(){
+  $("#date").removeClass("hidden");
+  $("#more_detail").removeClass("hidden");
+  $("#entry").removeClass("hidden");
+  $("#check").removeClass("hidden");
+  $("#date").html($(this).text());
+  $(".more").data("td", this);  
+  
+});
+
+  $('input[name="shift_checkbox"]').on({'switchChange.bootstrapSwitch': function(event, state) {
+      if(state == true ){
+        console.log("可");
+        $(".time").attr('disabled', false);
+        $($(".more").data("td")).addClass("select");
+      } else {
+        console.log("不可");
+        $(this).attr("id","select");
+        $(".time").attr('disabled', true);
+        $($(".more").data("td")).removeClass("select");
+      }
+  }});
+
+
+
