aa63bab2659f2a31ae675acf21af18bd5ace6c8f
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-08-29 22:25:52 +0900

    toggleできた。appjsがutf8じゃない

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ $(function(){
   $(document).foundation();
 
   $('table tr td').bind("touchend",function(ev){
-      alert($("#month").text() + $(this).text() + "時");
+      // alert($("#month").text() + $(this).text() + "時");
+      if($(this).attr("value")){
+          $("#shift_in").text($("#month").text() + $(this).text() + 'ああ'  );
+        } else {
+
+        }
   });
 });
