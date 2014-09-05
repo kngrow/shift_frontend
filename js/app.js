3faf0e6b4b0792adc549657f5b1ea9e44b3ffed6
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-09-05 18:08:38 +0900

    三角調整

--- js/app.js
+++ js/app.js
@@ -3,15 +3,24 @@ $(function(){
 
   $('table tr td').bind("touchend",function(ev){
       // alert($("#month").text() + $(this).text() + "時");
-      var l = $(this).position().left;
-      var w = $(this).width();
-      console.log("left:"+ l);
-      console.log("width" + w);
-      if($(this).attr("value")){
-          $("#shift_in").text($("#month").text() + $(this).text() + '時'  );
 
+      //leftとwidthを取ってきて。widthの1/2をたす
+      var left = $(this).position().left;
+      var width = $(this).width();
+      var trianglecenter = left + (width / 2) -10;
+
+      console.log("left:"+ left);
+      console.log("width" + width);
+      console.log("center" + trianglecenter);
+
+      if($(this).attr("value")){
+        $("#shift_in_allow").css("left", trianglecenter);
+        $("#shift_in").html(' <div id="shift_in_allow"></div>' +$("#month").text() + $(this).text() + '時'  );
+        $("#shift_in_allow").css("left", trianglecenter);
         } else {
-          $("#shift_in").text("今日はなし");
+          $("#shift_in_allow").css("left", trianglecenter);
+          $("#shift_in").html(' <div id="shift_in_allow"></div>' +'今日はなし');
+          $("#shift_in_allow").css("left", trianglecenter);
         }
   });
 });
