f59efe2f3d79d89c486d2290b6be7de28a1a7467
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-09-05 16:36:29 +0900

    width left get

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@ $(function(){
 
   $('table tr td').bind("touchend",function(ev){
       // alert($("#month").text() + $(this).text() + "時");
+      var l = $(this).position().left;
+      var w = $(this).width();
+      console.log("left:"+ l);
+      console.log("width" + w);
       if($(this).attr("value")){
           $("#shift_in").text($("#month").text() + $(this).text() + '時'  );
+
         } else {
           $("#shift_in").text("今日はなし");
         }
