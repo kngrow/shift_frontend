0bb58815efb97c724f763f40b02e6b24af009892
k.hamada <k.hamada.1988@gmail.com>
2014-09-12 08:36:17 +0900

    Change elements of call to open modal
    link("table tr td a") -> cell("table tr td")
    
    *   Disable hashTracking - <https://github.com/VodkaBears/Remodal#hashtracking>
    *   Call method - <https://github.com/VodkaBears/Remodal#methods>

--- js/entry.js
+++ js/entry.js
@@ -1,5 +1,6 @@
 $(function(){
-  $("table tr td a").on("click touchend", function(ev){
+  $("table tr td").on("click touchend", function(ev){
     $("#modal_month").html($("#month").text() + $(this).text() + "日");
+    $.remodal.lookup[$("[data-remodal-id=modal]").data("remodal")].open();
   });
 });
