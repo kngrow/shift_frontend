83e1e9af7da04669eb87c94f5116f57b86e83778
k.hamada <k.hamada.1988@gmail.com>
2014-09-12 08:24:38 +0900

    Fix insert modal title

--- js/entry.js
+++ js/entry.js
@@ -1,10 +1,5 @@
 $(function(){
-
-  $('table tr td a').bind("touchend",function(ev){
-        $("#modal_month").html($("#month").text() +$(this).text() + "日" );
+  $("table tr td a").on("click touchend", function(ev){
+    $("#modal_month").html($("#month").text() + $(this).text() + "日");
   });
-$(document).on('open', '.remodal', function () {
-    console.log('open');
-    // $("#modal_month").html($("#month").text() +$(this).text() + "日" );
-});
 });
