533d36322c68c9057a6c6e595c9f50b4d35960c6
k.hamada <k.hamada.1988@gmail.com>
2014-09-12 12:03:13 +0900

    Change remodal declarative style
    *   <https://github.com/VodkaBears/Remodal#cool-bro-but-i-dont-like-declarative-style>

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,8 @@
 $(function(){
+  var remodal = $("[data-remodal-id=modal]").remodal({hashTracking: false});
+
   $("table tr td").on("click touchend", function(ev){
     $("#modal_month").html($("#month").text() + $(this).text() + "日");
-    $.remodal.lookup[$("[data-remodal-id=modal]").data("remodal")].open();
+    remodal.open();
   });
 });
