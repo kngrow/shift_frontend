fc9eb0a0d17d981e7568d082967ad3b1a8cbd082
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-09-15 18:30:49 +0900

    modalの位置を修正

--- js/entry.js
+++ js/entry.js
@@ -1,8 +1,16 @@
 $(function(){
   var remodal = $("[data-remodal-id=modal]").remodal({hashTracking: false});
-
+  var select_day, select_start , select_end;
   $("table tr td").on("click touchend", function(ev){
-    $("#modal_month").html($("#month").text() + $(this).text() + "日");
+
+   select_day = $("#month").text() + $(this).text() + "日";
+    $("#modal_month").html( select_day);
     remodal.open();
   });
+  $(document).on('confirm', '.remodal', function () {
+   select_start = $("#select_start").val();
+    console.log(select_start);
+    $("#shift_selected").append( "<div>" +select_day + select_start +"</div>" );
+
+});
 });
